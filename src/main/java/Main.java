8c33fd958c434492ab8fb7082fd8389494356481
AF <abolfazlnabavi6831@gmail.com>
2026-07-11 21:30:30 +0330

    some changes in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,29 @@
+package com.silicontycoon;
+
+import com.silicontycoon.controller.GameController;
+import com.silicontycoon.view.GameView;
+import com.silicontycoon.view.SetupDialog;
 import javafx.application.Application;
 import javafx.stage.Stage;
-import view.scene.GameScene;
 
+/**
+ * Application entry point. Shows the pre-game setup dialog, then hands
+ * off to {@link GameView} once the player count / names / map size are
+ * confirmed. Console-only execution is intentionally not supported:
+ * the assignment requires a JavaFX GUI.
+ */
 public class Main extends Application {
-  
+
     @Override
     public void start(Stage primaryStage) {
-        f(primaryStage);
+        SetupDialog.show(primaryStage, (names, mapSize) -> {
+            GameController controller = new GameController(names, mapSize);
+            GameView view = new GameView(primaryStage, controller);
+            view.show();
+        });
     }
 
-    public static void f(Stage primaryStage) {
-        primaryStage.setTitle("SILICON VALLEY");
-        primaryStage.setHeight(1024);
-        primaryStage.setWidth(1024);
-        primaryStage.setResizable(false);
-
-        GameScene gameScene = new GameScene();
-        primaryStage.setScene(gameScene.init());
-        primaryStage.show();
+    public static void main(String[] args) {
+        launch(args);
     }
-
 }
