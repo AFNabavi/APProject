685d90caef0820e0697d6f24a312413dabeaf801
AF <abolfazlnabavi6831@gmail.com>
2026-07-11 21:27:36 +0330

    Exception is completed

---
+++ src/main/java/exception/InsufficientResourcesException.java
@@ -0,0 +1,11 @@
+package com.silicontycoon.exception;
+
+/**
+ * Thrown when a player attempts an action (build, upgrade, trade) that
+ * costs more resources than they currently hold.
+ */
+public class InsufficientResourcesException extends GameException {
+    public InsufficientResourcesException(String message) {
+        super(message);
+    }
+}
